cf4c0d6aaf6bb35a839bb4dffd1cb0396cf33ef8
Andrei Dailida <2890370@gmail.com>
2018-05-24 02:12:43 +0200

    Begining

---
+++ Deck.py
@@ -0,0 +1,32 @@
+import random
+
+class Deck:
+
+    cardsDeck = ["7c", "7d", "7h", "7s",
+                 "8c", "8d", "8h", "8s",
+                 "9c", "9d", "9h", "9s",
+                 "10c", "10d", "10h", "10s",
+                 "Jc", "Jd", "Jh", "Js",
+                 "Qc", "Qd", "Qh", "Qs",
+                 "Kc", "Kd", "Kh", "Ks",
+                 "Ac", "Ad", "Ah", "As"]
+
+    def __init__(self):
+        random.shuffle(self.cardsDeck)
+
+    def shuffleDeck(self):
+        random.shuffle(self.cardsDeck)
+
+    def giveCard(self):
+        #if (len(self.cardsDeck) == 0):
+            #getTableDiscard
+        i = random.randint(0, len(self.cardsDeck)-1)
+        newCard = self.cardsDeck[i]
+        del self.cardsDeck[i]
+        return newCard
+
+c = Deck()
+for i in range (0, 32):
+    print(c.cardsDeck)
+    print(c.giveCard())
+print(c.cardsDeck)
---
+++ Player.py
@@ -0,0 +1,16 @@
+class Player:
+
+    cardsAvail = []             # Cards availiable in player hands
+
+    def takeCard(self):         # To take cards form deck
+        x=10
+
+    def passTurn(self):         # To pass turn
+        x=10
+
+    def makeTurn(self, n):      # To make turn with card "n" form cardsAvial[]
+        x=10
+
+    def showCards(self):        # Do display availiable cards
+        x=10
+        
